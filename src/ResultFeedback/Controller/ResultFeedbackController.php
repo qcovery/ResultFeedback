472fe39949bd5d03eb5b99d0c008d82463ec78a6
Oliver Stöhr <stoehr@effective-webwork.de>
2023-07-03 12:05:00 +0200

    Replace Zend with Laminas

--- src/ResultFeedback/Controller/ResultFeedbackController.php
+++ src/ResultFeedback/Controller/ResultFeedbackController.php
@@ -13,7 +13,7 @@
 namespace ResultFeedback\Controller;
 
 use VuFind\Exception\Mail as MailException;
-use Zend\Mail\Address;
+use Laminas\Mail\Address;
 
 /**
  * Feedback Class
@@ -31,7 +31,7 @@ class ResultFeedbackController extends \VuFind\Controller\AbstractBase
     /**
      * Display Feedback home form.
      *
-     * @return \Zend\View\Model\ViewModel
+     * @return \Laminas\View\Model\ViewModel
      */
     public function homeAction()
     {
@@ -46,7 +46,7 @@ class ResultFeedbackController extends \VuFind\Controller\AbstractBase
      */
     public function emailAction()
     {
-        $translator = $this->serviceLocator->get('Zend\Mvc\I18n\Translator');
+        $translator = $this->serviceLocator->get('Laminas\Mvc\I18n\Translator');
 
         $view = $this->createViewModel();
         $view->useRecaptcha = $this->recaptcha()->active('feedback');
